6ca51150d599669a597c1b99a753cc63d0c1ab23
Cole Reveal <alexi.reveal@gmail.com>
2014-05-30 14:13:24 -0500

    added upload capabilities with type check

--- public/todo_list.php
+++ public/todo_list.php
@@ -17,9 +17,8 @@
 				
 
 		// open in a file
-		function open_file() {
+		function open_file($file = 'todo_list.txt') {
 		    $list = [];
-		    $file = 'todo_list.txt';
 		    if (is_readable($file)) {
 		        
 		        $filesize = filesize($file);
@@ -41,22 +40,57 @@
 </head>
 <body>
 
+
 	<h1>TODO List</h1>
 	<!--this is all the stuff for the list-->	
 	<?php 
+
 		$todos = open_file();
 		
 		//remove todos
 		if(!empty($_GET)){
-			unset($todos[$_GET['index']]);
+			if ($_GET['action'] == 'remove'){
+				unset($todos[$_GET['index']]);
+				header("Location: /todo_list.php");
+				save($todos,'todo_list.txt');
+				exit(0);
+			}
 		}
 		
 		save($todos,'todo_list.txt');
 		
 		//add todos
 		if(!empty($_POST)){
-			$todos[] = $_POST['new_items']; 
+			$todos[] = $_POST['new_items'];
+			header("Location: /todo_list.php");
+			save($todos,'todo_list.txt');
+			exit(0); 
 		}
+
+		
+
+		// Verify there were uploaded files and no errors
+		if (count($_FILES) > 0 && $_FILES['files']['error'] == 0 && $_FILES['files']['type'] == 'text/plain') {
+			
+			// Set the destination directory for uploads
+			$upload_dir = '/vagrant/sites/todo.dev/public/uploads/';
+			// Grab the filename from the uploaded file by using basename
+			$filename = basename($_FILES['files']['name'] . time());
+			// Create the saved filename using the file's original name and our upload directory
+			$saved_filename = $upload_dir . $filename;
+			// Move the file from the temp location to our uploads directory
+			move_uploaded_file($_FILES['files']['tmp_name'], $saved_filename);
+		}
+
+		if(isset($saved_filename)){
+			$imported_list = [];	
+			$imported_list = open_file("uploads/$filename");
+			$todos = array_merge($todos, $imported_list);
+			save($todos,'todo_list.txt');
+		}
+
+
+		
 		
 		//write list
 		if(!empty(implode($todos))){			
@@ -76,7 +110,7 @@
 	<!--add itmes here-->	
 	<h2>Add New Items</h2>
 	<form method="POST" action="todo_list.php">
-		<textarea name="new_items" autofocus></textarea>
+		<input type="text" name="new_items" autofocus>
 		<p>
 			<input type="submit">
 		</p>
@@ -85,6 +119,24 @@
 	<?php	
 		save($todos,'todo_list.txt');
 	?>
+
+	<?php
+
+	if (isset($_FILES['files']['type']) && $_FILES['files']['type'] != 'text/plain') {
+		echo "<h1 style='color: red'>File must be a text file... You jive turkey!!!</h1>";
+	}
+	?>
+
+
+	<h2>File Upload</h2>
+
+	<form method="POST" action="/todo_list.php" enctype="multipart/form-data">
+		<label for="file">File to Upload:</label>
+		<input type="file" id="file" name="files">
+		<p>
+			<input type="submit" vaule="Upload">
+		</p>
+	</form>	
 
 </body>
 </html>

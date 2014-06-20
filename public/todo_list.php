5562988f8da2c07d98d65a3e8503e6a01cebc8b7
Cole Reveal <alexi.reveal@gmail.com>
2014-06-20 11:39:55 -0500

    added exception catching to todo list

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,6 +1,4 @@
-<?
-
-	
+<?	
 $errormessage = '';
 require_once('filestore.php');
 
@@ -27,20 +25,19 @@ $open->write($todos);
 // save($todos,'todo_list.txt');
 
 //add todos
-if(!empty($_POST)){
+try{
+	if(!empty($_POST)){
 
-	if(strlen($_POST['new_items']) > 240) {
-		throw new Exception('Dude that was too long. Try a shorter todo item.');
+		if(strlen($_POST['new_items']) > 240) {
+			throw new Exception("We're sorry. The input provided was too long. Please try again.");
+		}
+		//take in new item added
+		$todos[] = $_POST['new_items'];
+		//save bc we can
+		$open->write($todos);
 	}
-	//take in new item added
-	$todos[] = $_POST['new_items'];
-	//reload page
-	header("Location: /todo_list.php");
-	//save bc we can
-	$open->write($todos);
-	// save($todos,'todo_list.txt');
-	//no mas
-	exit(0); 
+}catch(Exception $e){
+	$msg = $e->getMessage();
 }
 
 	
@@ -60,12 +57,10 @@ if (count($_FILES) > 0 && $_FILES['files']['error'] == 0) {
 		$import = new Filestore("uploads/$filename");
 		//read in file
 		$imported_list = $import->read();
-		// $imported_list = open_file("uploads/$filename");
 		//add new items to todo list
 		$todos = array_merge($todos, $imported_list);
 		//save
 		$open->write($todos);
-		// save($todos,'todo_list.txt');
 	} else {
 		//send error message if not a text file
 		$errormessage = "File must be a text file... You jive turkey!!!";		
@@ -86,13 +81,17 @@ if (count($_FILES) > 0 && $_FILES['files']['error'] == 0) {
 	<h1>TODO List</h1>
 	<ul>	
 		<?	
-			//write list
-			if(!empty(implode($todos))):			
-				//for each todo item, write it out and give it a remove link
-				foreach ($todos as $key => $todo):?>
-					<li class="show_up"><?= htmlspecialchars(strip_tags(($todo)))?> <a class="complete"href=<?="?action=remove&index=$key"?>>Mark Complete</a></li>
-				<?endforeach;		
-			endif; ?>
+			try {//write list
+				if(!empty(implode($todos))):			
+					//for each todo item, write it out and give it a remove link
+					foreach ($todos as $key => $todo):?>
+						<li class="show_up"><?= htmlspecialchars(strip_tags(($todo)))?> <a class="complete"href=<?="?action=remove&index=$key"?>>Mark Complete</a></li>
+					<?endforeach;		
+				endif; 
+			}catch(Exception $e) {
+				echo "We're sorry. The input provided was too long. Please try again.";
+			}
+		?>
 	</ul>	
 	<!--this is all the stuff for the list-->	
 
@@ -100,6 +99,11 @@ if (count($_FILES) > 0 && $_FILES['files']['error'] == 0) {
 	<!--add itmes here-->	
 	<h2>Add New Items</h2>
 	
+	<?
+		if(isset($msg)) : ?>
+			<?= "<h2 style='color:red'>$msg</h2>" ?>
+		<? endif; 
+	?>
 
 	<form method="POST" action="todo_list.php">
 		<input id="add_item_box"type="text" name="new_items">
@@ -113,8 +117,10 @@ if (count($_FILES) > 0 && $_FILES['files']['error'] == 0) {
 
 	<?
 		if(isset($errormessage)) : ?>
-			<?= "<h1 style='color:red'>$errormessage</h1>" ?>
-		<? endif; ?>
+			<?= "<h2 style='color:red'>$errormessage</h2>" ?>
+		<? endif; 
+	?>
+
 
 	<!--accepts file uplaod input-->
 	<form method="POST" action="/todo_list.php" enctype="multipart/form-data">
